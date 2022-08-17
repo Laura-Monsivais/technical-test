3429fa68a78f90e50e729085a2cc5baea8453297
Laura <laura.monsivais@webppi.com>
2022-08-17 09:54:07 -0500

    first commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        dd("hola");
+        $this->validateLogin($request);
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+        return response()->json([
+            'token' => $request->user()->createToken($request->device)->plainTextToken,
+            'message' => 'Success'
+        ]);
+    }
+    public function validateLogin(Request $request)
+    {
+        return $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device' => 'required'
+        ]);
+    }
+}
